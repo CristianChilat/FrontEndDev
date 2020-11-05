d2ca5ac5ed19111f5d252b5319dbb6de710bf4ba
Cristian <chilatcristian@gmail.com>
2020-11-05 20:35:49 +0200

    JS working

---
+++ Site/4-Lectie JS/JS-1/app.js
@@ -0,0 +1,27 @@
+console.log("Hello world!");
+
+let a = 4;
+let b = "mesaj";
+let c;
+let d = false;
+let e = null;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.clear;
+
+a = 10;
+b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a%b)
+
+console.clear;
+ 
---
+++ Site/4-Lectie JS/JS-2/script.js
@@ -0,0 +1,37 @@
+let n = -5;
+
+if(n > -1 && n < 10){
+    console.log("Aceasta este o cifra")
+}   else{
+    console.log("Aceasta nu este o cifra")
+}   
+
+console.clear();
+
+let cifra = 3;
+
+if(cifra == 10){
+    console.log("Zece")
+}else if(cifra == 9){
+    console.log("Noua")
+}else if(cifra == 8){
+    console.log("Opt")
+}else if(cifra == 7){
+    console.log("Sapte")
+}else if(cifra == 6){
+    console.log("Sase")
+}else if(cifra == 5){
+    console.log("Cinci")
+}else if(cifra == 4){
+    console.log("Patru")
+}else if(cifra == 3){
+    console.log("Trei")
+}else if(cifra == 2){
+    console.log("Doi")
+}else if(cifra == 1){
+    console.log("Unu")
+}else if(cifra == 0){
+    console.log("Zero")
+}else{
+    console.log("Nu este o cifra")
+}
